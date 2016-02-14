91f7650661e6819162ada61851dba08b1737b082
Yarny998 <yarnold998@gmail.com>
2016-02-14 22:08:36 +1100

    Minor updates

--- Packages/AlsaEqual/alsaequal.sh
+++ Packages/AlsaEqual/alsaequal.sh
@@ -35,20 +35,24 @@ sed 's/module = "Eq"/module = "Eq10"/' -i pcm_equal.c
 make
 install -dm755 "/usr/lib/alsa-lib"
 make install
+cp /usr/lib/alsa-lib/*so /usr/lib/arm-linux-gnueabihf/alsa-lib
 
 #Cleanup
 cd /tmp
-#rm $pkgname-$pkgver.tar.bz2
-#rm -r $pkgname
+rm $pkgname-$pkgver.tar.bz2
+rm -r $pkgname
 
 #Get the config file and service file
 wget https://raw.githubusercontent.com/Yarny998/MPD-Jesse-Lite/master/Packages/AlsaEqual/asound.conf
-wget https://raw.githubusercontent.com/Yarny998/MPD-Jesse-Lite/master/Packages/AlsaEqual/.alsaequal.bin.std
+wget https://raw.githubusercontent.com/Yarny998/MPD-Jesse-Lite/master/Packages/AlsaEqual/.alsaequal.bin.1
 
 # Install files
 install -D -m644 asound.conf "/etc/asound.conf"
-install -D -m644 .alsaequal.bin.std "/var/lib/mpd/.alsaequal.bin.std"
+install -D -m644 .alsaequal.bin.1 "/var/lib/mpd/.alsaequal.bin.1"
+
+#make a copy of the standard settings
+cp /var/lib/mpd/.alsaequal.bin /var/lib/mpd/.alsaequal.bin.std
 
 #Cleanup
 rm asound.conf
-rm .alsaequal.bin.std
+rm .alsaequal.bin.1
