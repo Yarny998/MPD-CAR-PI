73a5c4eea84a37649a75976183a682d2bc83182a
Yarny998 <yarnold998@gmail.com>
2016-02-14 11:57:57 +1100

    Create mpd.sh
    Script for installing MPD MPC and using custom config file based on
    ArchPhile

---
+++ Packages/MPD/mpd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+## MPD
+## This is the full installation script including compilation
+#
+# Website http://www.musicpd.org/
+#
+# Configuration file based on ARCHPHILE
+
+pkgname=mpd
+pkgver=1.2.3
+pkgrel=1
+
+#Dependencies
+aptitude install mpd mpc
+
+cd /tmp
+
+#Get the config file and service file
+wget https://raw.githubusercontent.com/Yarny998/MPD-Jesse-Lite/master/Packages/mpd/mpd.conf
+
+# Install config files
+install -Dm644  mpd.conf /etc/mpd.conf
+
+#Enable and start the service
+systemctl restart mpd.service
+systemctl status mpd.service
+
+#Cleanup
+rm mpd.conf
