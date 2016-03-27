2840cef70327bcbf7441ebcb5ff98cc31b946b22
Yarny998 <yarnold998@gmail.com>
2016-03-27 20:44:27 +1100

    Include graphical setup
    Included LXDE and a number of other X packages required

--- Packages/Coverart/setup.sh
+++ Packages/Coverart/setup.sh
@@ -5,11 +5,13 @@
 #
 ## This installs a number of files to show coverart and other backgrounds
 ## while MPD plays music files
+#
+## Installs and sets up LXDE as well
 
 pkgname=coverart
 pkgver=1.0
 pkgrel=1
-declare -a depends=('x11-xserver-utils' 'gxmessage' 'imagemagick')
+declare -a depends=('xinit' 'lxde-core' 'lxterminal' 'lxappearance' 'lightdm' 'iceweasel' 'x11-xserver-utils' 'gxmessage' 'imagemagick')
 
 echo "Installing $pkgname"
 
@@ -41,6 +43,25 @@ install -D -m755 -o=mpd -g=audio coverart.sh /var/lib/mpd/coverart.sh
 install -D -m755 -o=mpd -g=audio YMPD /var/lib/mpd/Desktop/YMPD
 install -D -m644 -o=mpd -g=audio autostart /var/lib/mpd/.config/lxsession/LXDE/autostart
 tar -xf art.tar /var/lib/mpd
+
+# Setting graphical.target as the new default target
+if command -v systemctl > /dev/null && systemctl | grep -q '\-\.mount'; then
+    SYSTEMD=1
+elif [ -f /etc/init.d/cron ] && [ ! -h /etc/init.d/cron ]; then
+    SYSTEMD=0
+else
+    echo "Unrecognised init system"
+    return 1
+fi
+
+if [ $SYSTEMD -eq 1 ]; then
+    systemctl set-default graphical.target
+    ln -fs /etc/systemd/system/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+else
+    update-rc.d lightdm enable 2
+fi
+
+sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=mpd/"
 
 #Cleanup
 cd /tmp
